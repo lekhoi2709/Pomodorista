3a27085603d38a3622e2d23003fadd3136daedb6
lekhoi2709 <ledinhkhoimd@gmail.com>
2022-12-04 12:20:36 +0700

    add all files

---
+++ app/src/main/java/tdtu/lab/afinal/MainActivity.java
@@ -0,0 +1,114 @@
+package tdtu.lab.afinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Locale;
+
+
+public class MainActivity extends AppCompatActivity {
+    public long startTime;
+    private boolean isRunning = false;
+    private long timeLeft;
+    TextView tv_CountDown;
+    Button btn_play, btn_refresh, btn_setting;
+    CountDownTimer timer;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences pref = getSharedPreferences("StartTime", Context.MODE_PRIVATE);
+        startTime = pref.getLong("start_time", 600000);
+        timeLeft = startTime;
+
+        tv_CountDown = findViewById(R.id.tv_Countdown);
+        btn_play = findViewById(R.id.btn_play);
+        btn_refresh = findViewById(R.id.btn_refresh);
+        btn_setting = findViewById(R.id.btn_setting);
+        progressBar = findViewById(R.id.progressBar);
+
+        btn_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent settingIntent = new Intent(MainActivity.this, SettingActivity.class);
+                MainActivity.this.startActivity(settingIntent);
+            }
+        });
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isRunning){
+                    startTimer(progressBar, startTime);
+                    btn_play.setBackgroundResource(R.drawable.ic_btn_pause);
+                } else {
+                    pauseTimer();
+                    btn_play.setBackgroundResource(R.drawable.ic_btn_start);
+                }
+            }
+        });
+
+        btn_refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer(progressBar, startTime);
+            }
+        });
+        countDownText();
+    }
+
+    public void startTimer(ProgressBar progressBar, long startTime){
+        timer = new CountDownTimer(timeLeft, 1000){
+            @Override
+            public void onTick(long timeLength) {
+                timeLeft = timeLength;
+                progressBar.setProgress((int)(timeLeft * 100 / startTime));
+                countDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                isRunning = false;
+                progressBar.setProgress(0);
+                Toast.makeText(getApplicationContext(), "Count down timer has ended.", Toast.LENGTH_LONG).show();
+            }
+        }.start();
+        isRunning = true;
+    }
+
+    public void pauseTimer(){
+        timer.cancel();
+        isRunning = false;
+    }
+
+    public void resetTimer(ProgressBar progressBar, long startTime){
+        if(timer != null){
+            timeLeft = startTime;
+            progressBar.setProgress((int)(timeLeft * 100 / startTime));
+            pauseTimer();
+            countDownText();
+            isRunning = false;
+            btn_play.setBackgroundResource(R.drawable.ic_btn_start);
+        }
+    }
+
+    public void countDownText(){
+        int min = (int)(timeLeft / 1000) / 60;
+        int sec = (int)(timeLeft / 1000) % 60;
+
+        String stringTimer = String.format(Locale.ENGLISH, "%02d : %02d", min, sec);
+        tv_CountDown.setText(stringTimer);
+    }
+}
---
+++ app/src/main/java/tdtu/lab/afinal/SettingActivity.java
@@ -0,0 +1,96 @@
+package tdtu.lab.afinal;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+public class SettingActivity extends AppCompatActivity {
+    Button btn_goBack;
+    SwitchMaterial sw_mode;
+    SeekBar seekBar;
+    TextView tv_setTime;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        btn_goBack = findViewById(R.id.btn_goBack);
+        sw_mode = findViewById(R.id.sw_mode);
+        seekBar = findViewById(R.id.seekBar);
+        tv_setTime = findViewById(R.id.tv_setTime);
+        SharedPreferences pref = getSharedPreferences("StartTime", Context.MODE_PRIVATE);
+
+        int progress =(int)(pref.getLong("start_time", 10)/1000/60);
+        seekBar.setMax(99);
+        seekBar.setProgress(progress);
+        tv_setTime.setText(progress + " mins");
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int time, boolean b) {
+                long start_time = (long) seekBar.getProgress()*60*1000;
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putLong("start_time", start_time);
+                editor.commit();
+                update(time);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        boolean isDark = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES;
+        String dark = "Dark Mode";
+        String light = "Light Mode";
+
+        btn_goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(SettingActivity.this, MainActivity.class);
+                SettingActivity.this.startActivity(homeIntent);
+            }
+        });
+
+        if(isDark){
+            sw_mode.setText(light);
+        } else {
+            sw_mode.setText(dark);
+        }
+
+        sw_mode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    sw_mode.setText(light);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    sw_mode.setText(dark);
+                }
+            }
+        });
+    }
+
+    public void update(int time){
+        String set_time = time + " mins";
+        tv_setTime.setText(set_time);
+    }
+}
